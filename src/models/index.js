86e161e11f1d62d1db62d1c03c77048519b4e20b
Rruben555 <rubensiregar25@gmail.com>
2025-12-06 02:05:22 +0700

    Fix index

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,50 @@
+// src/models/index.js
 import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 
+import userModel from "./user.js";
+import characterModel from "./character.js";
+import weaponModel from "./weapon.js";
+import postModel from "./post.js";
+import commentModel from "./comment.js";
+
 dotenv.config();
 
+// Gunakan DATABASE_URL di Railway
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  logging: false,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
   },
-  logging: false,
 });
+
+// INIT MODELS
+export const User = userModel(sequelize, DataTypes);
+export const Character = characterModel(sequelize, DataTypes);
+export const Weapon = weaponModel(sequelize, DataTypes);
+export const Post = postModel(sequelize, DataTypes);
+export const Comment = commentModel(sequelize, DataTypes);
+
+// RELATIONS
+User.hasMany(Post, { foreignKey: "user_id" });
+Post.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(Comment, { foreignKey: "user_id" });
+Comment.belongsTo(User, { foreignKey: "user_id" });
+
+Post.hasMany(Comment, { foreignKey: "post_id", onDelete: "CASCADE" });
+Comment.belongsTo(Post, { foreignKey: "post_id" });
+
+// EXPORT SEMUA (JANGAN ADA default)
+export {
+  sequelize,
+  User,
+  Character,
+  Weapon,
+  Post,
+  Comment,
+};
