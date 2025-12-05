b79c00057e966a9cfc98fbb5d390ad7f12e5a22f
Rruben555 <rubensiregar25@gmail.com>
2025-12-06 01:45:13 +0700

    Backend to Postgre Test

--- src/database.js
+++ src/database.js
@@ -15,14 +15,15 @@
 // );
 
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  logging: false,
+  dialectOptions: {
+    ssl: false
+  }
 });
 
+export default sequelize;
 
 // const {Pool} =require("pg");
 // const dotenv=require("dotenv");
