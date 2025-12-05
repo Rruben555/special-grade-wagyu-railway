5d99a4884f68f41780b719023695bfa93c27c4c0
Rruben555 <rubensiregar25@gmail.com>
2025-12-06 02:02:04 +0700

    Update index

--- src/models/index.js
+++ src/models/index.js
@@ -1,50 +1,15 @@
 import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 
-import userModel from "./user.js";
-import characterModel from "./character.js";
-import weaponModel from "./weapon.js";
-import postModel from "./post.js";
-import commentModel from "./comment.js";
-
 dotenv.config();
 
-// ---- Gunakan DATABASE_URL langsung (Private URL dari Railway) ----
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
-    ssl: false, // ⚠️ Railway private URL tidak pakai SSL
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
   logging: false,
 });
-
-// ---- Models ----
-export const User = userModel(sequelize, DataTypes);
-export const Character = characterModel(sequelize, DataTypes);
-export const Weapon = weaponModel(sequelize, DataTypes);
-export const Post = postModel(sequelize, DataTypes);
-export const Comment = commentModel(sequelize, DataTypes);
-
-// ---- User ↔ Post ----
-User.hasMany(Post, { foreignKey: "user_id" });
-Post.belongsTo(User, { foreignKey: "user_id" });
-
-// ---- User ↔ Comment ----
-User.hasMany(Comment, { foreignKey: "user_id" });
-Comment.belongsTo(User, { foreignKey: "user_id" });
-
-// ---- Post ↔ Comment ----
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
-  onDelete: "CASCADE",
-});
-Comment.belongsTo(Post, { foreignKey: "post_id" });
-
-export default {
-  sequelize,
-  User,
-  Character,
-  Weapon,
-  Post,
-  Comment,
-};
